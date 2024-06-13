3ae86c8a82a6d4a70bd95bb67008c34d6220c9e7
Wosmerik Alexandr <shurik1777@gmail.com>
2024-06-13 19:48:01 +0300

    семинар 4 сделали первые формы

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -29,5 +29,6 @@ urlpatterns = [
     # path('wm/', include('wmodel.urls')),
     path('lec/', include('lection3.urls')),
     # path('', index),
-    path('f/', include('lec_forms.urls'))
+    path('f/', include('lec_forms.urls')),
+    path('fs/', include('form_seminar.urls'))
 ]
