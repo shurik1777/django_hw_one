a0404d36d97408fec02bba3bd1bc4bc61f8bab5f
Wosmerik Alexandr <shurik1777@gmail.com>
2024-06-15 11:17:59 +0300

    добавил модель Клиента с логикой на будущее

--- myproject/hw_logik_and_template/models.py
+++ myproject/hw_logik_and_template/models.py
@@ -1,3 +1,22 @@
-from django.db import models
+from django.db import models as m
 
-# Create your models here.
+
+class Client(m.Model):
+    """
+    name - имя,
+    email - эл. почта(уникальна),
+    phone - номер телефона
+    date_of_register - дата регистрации(автоматически срабатывает если не указать)
+    """
+    name = m.CharField(max_length=32)
+    email = m.EmailField(unique=True)
+    phone = m.CharField(max_length=15)
+    address = m.CharField(max_length=128)
+    date_of_register = m.DateField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        self.name = self.name.capitalize()
+        super(Client, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
